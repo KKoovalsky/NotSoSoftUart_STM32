b4484fd39f914e17c4b4ebdffb9688f626d9b1f0
Kacper <kacper.s.kowalski@gmail.com>
2018-02-10 12:51:50 +0100

    Enable EXTI peripheral clock.

--- app/src/hw/gpio.c
+++ app/src/hw/gpio.c
@@ -68,7 +68,7 @@ void MX_GPIO_Init(void)
 	/* GPIO Ports Clock Enable */
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-
+	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
   	/**/
   	LL_GPIO_ResetOutputPin(D5_SOFTUART_OUT_GPIO_Port, D5_SOFTUART_OUT_Pin);
 
